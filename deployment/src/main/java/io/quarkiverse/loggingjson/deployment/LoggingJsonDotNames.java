3d3d8148cf6832b5696920c6a304c88dcb2194be
Simon Bengtsson <SlyngDK@users.noreply.github.com>
2020-12-16 12:45:33 +0100

    Fixed issue where JsonProviders where optimized away by quarkus.

---
+++ deployment/src/main/java/io/quarkiverse/loggingjson/deployment/LoggingJsonDotNames.java
@@ -0,0 +1,11 @@
+package io.quarkiverse.loggingjson.deployment;
+
+import org.jboss.jandex.DotName;
+
+import io.quarkiverse.loggingjson.JsonProvider;
+
+class LoggingJsonDotNames {
+
+    static final DotName JSON_PROVIDER = DotName.createSimple(JsonProvider.class.getName());
+
+}

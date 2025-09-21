819d2830637b0b4178c5b7bf467fb4570f1925b3
Ivan Puntev <naka3a7el@gmail.com>
2025-09-21 20:36:42 +0300

    Adding possibility to load jackson modules available in the classpath.

--- deployment/src/main/java/io/quarkiverse/loggingjson/deployment/LoggingJsonProcessor.java
+++ deployment/src/main/java/io/quarkiverse/loggingjson/deployment/LoggingJsonProcessor.java
@@ -2,13 +2,17 @@ package io.quarkiverse.loggingjson.deployment;
 
 import java.util.Collection;
 
+import jakarta.inject.Inject;
+
 import org.jboss.jandex.ClassInfo;
 
 import io.quarkiverse.loggingjson.JsonFactory;
 import io.quarkiverse.loggingjson.LoggingJsonRecorder;
+import io.quarkiverse.loggingjson.deployment.config.ConfigJackson;
 import io.quarkiverse.loggingjson.jackson.JacksonJsonFactory;
 import io.quarkiverse.loggingjson.jsonb.JsonbJsonFactory;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.bootstrap.classloading.QuarkusClassLoader;
 import io.quarkus.deployment.Capabilities;
 import io.quarkus.deployment.Capability;
 import io.quarkus.deployment.annotations.BuildProducer;
@@ -20,10 +24,14 @@ import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.LogConsoleFormatBuildItem;
 import io.quarkus.deployment.builditem.LogFileFormatBuildItem;
 import io.quarkus.deployment.builditem.LogSocketFormatBuildItem;
+import io.quarkus.jackson.spi.ClassPathJacksonModuleBuildItem;
 
 class LoggingJsonProcessor {
 
     private static final String FEATURE = "logging-json";
+
+    @Inject
+    ConfigJackson configJackson;
 
     @BuildStep
     FeatureBuildItem feature() {
@@ -46,6 +54,15 @@ class LoggingJsonProcessor {
     @Record(ExecutionTime.RUNTIME_INIT)
     LogSocketFormatBuildItem setUpSocketFormatter(Capabilities capabilities, LoggingJsonRecorder recorder) {
         return new LogSocketFormatBuildItem(recorder.initializeSocketJsonLogging(jsonFactory(capabilities)));
+    }
+
+    @BuildStep
+    void autoRegisterModules(BuildProducer<ClassPathJacksonModuleBuildItem> classPathJacksonModules) {
+        for (String moduleClassName : configJackson.modules()) {
+            if (QuarkusClassLoader.isClassPresentAtRuntime(moduleClassName)) {
+                classPathJacksonModules.produce(new ClassPathJacksonModuleBuildItem(moduleClassName));
+            }
+        }
     }
 
     private JsonFactory jsonFactory(Capabilities capabilities) {

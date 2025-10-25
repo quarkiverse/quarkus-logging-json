c4fc04de497f66c96bce16bbd179bf715014e991
troosan <antoine.de.troostembergh@gmail.com>
2025-10-25 14:45:39 +0200

    Cleanup test code reuse (#376)
    Avoid code duplication and make methods reusable by new test classes.

--- runtime/src/test/java/io/quarkiverse/loggingjson/providers/SequenceJsonProviderJsonbTest.java
+++ runtime/src/test/java/io/quarkiverse/loggingjson/providers/SequenceJsonProviderJsonbTest.java
@@ -19,17 +19,7 @@ public class SequenceJsonProviderJsonbTest extends JsonProviderBaseTest {
 
     @Test
     void testDefaultConfig() throws Exception {
-        final Config.FieldConfig config = new Config.FieldConfig() {
-            @Override
-            public Optional<String> fieldName() {
-                return Optional.empty();
-            }
-
-            @Override
-            public Optional<Boolean> enabled() {
-                return Optional.empty();
-            }
-        };
+        final Config.FieldConfig config = fieldConfig(Optional.empty(), Optional.empty());
         final SequenceJsonProvider provider = new SequenceJsonProvider(config);
 
         final ExtLogRecord event = new ExtLogRecord(Level.ALL, "", "");
@@ -43,19 +33,7 @@ public class SequenceJsonProviderJsonbTest extends JsonProviderBaseTest {
 
     @Test
     void testCustomConfig() throws Exception {
-        final var config = new Config.FieldConfig() {
-            private Optional<Boolean> enabled = Optional.of(false);
-
-            @Override
-            public Optional<String> fieldName() {
-                return Optional.of("seq");
-            }
-
-            @Override
-            public Optional<Boolean> enabled() {
-                return enabled;
-            }
-        };
+        Config.FieldConfig config = fieldConfig(Optional.of("seq"), Optional.of(false));
         final SequenceJsonProvider provider = new SequenceJsonProvider(config);
 
         final ExtLogRecord event = new ExtLogRecord(Level.ALL, "", "");
@@ -66,7 +44,7 @@ public class SequenceJsonProviderJsonbTest extends JsonProviderBaseTest {
         Assertions.assertEquals(4356, seq);
         Assertions.assertFalse(provider.isEnabled());
 
-        config.enabled = Optional.of(true);
+        config = fieldConfig(Optional.of("seq"), Optional.of(true));
         Assertions.assertTrue(new SequenceJsonProvider(config).isEnabled());
     }
 }
